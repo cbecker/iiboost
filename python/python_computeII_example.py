7cb334202a423d22ecb1f4a1e1ba9385ca30d189
Pol Monso Purti <pol.monso@epfl.ch>
2014-06-23 22:54:37 +0200

    Rebased Pol's IntegralImage PR

---
+++ python/python_computeII_example.py
@@ -0,0 +1,40 @@
+###################################################################################
+# Test for the IIBoost wrapper class
+###################################################################################
+
+from IIBoost import Booster
+from sklearn.externals import joblib	# to load data
+
+import numpy as np
+
+# to show something
+import matplotlib.pyplot as plt
+
+
+# load data
+gt = joblib.load("gt.jlb")
+img = joblib.load("img.jlb")
+
+imgFloat = np.float32(img)
+
+iiComputer = Booster()
+
+iiImage = iiComputer.computeIntegralImage( imgFloat )
+
+print "And now for the show."
+
+# show image & prediction side by side
+plt.ion()
+plt.figure()
+
+plt.subplot(1,2,1)
+plt.imshow(imgFloat[:,:,10],cmap="gray")
+plt.title("Click on any image to exit")
+
+plt.subplot(1,2,2)
+plt.imshow(iiImage[:,:,10],cmap="gray")
+plt.title("Click on any image to exit")
+
+plt.ginput(1)
+
+#print "Serialization string: " + model.serialize()

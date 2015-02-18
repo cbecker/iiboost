37b59813be1af3de34171382666fa8ddfa882548
Stuart Berg <bergs@janelia.hhmi.org>
2015-02-18 16:23:16 +0200

    tests: Added some more print statements.

--- python/tests/python_test_raw.py
+++ python/tests/python_test_raw.py
@@ -53,11 +53,13 @@ model = ctypes.c_void_p(
  					ctypes.c_int(numStumps),
  					ctypes.c_int(debugOutput) ) )
 
+print "--- Serializing model ---"
 serStr = ctypes.py_object( boostLib.serializeModel( model ) )
 
 # pre-alloc prediction
 pred = np.empty_like( imgs[0], dtype=np.dtype("float32") )
 
+print "--- Predicting ---"
 boostLib.predict( model, 
 				  ctypes.c_void_p(imgs[0].ctypes.data), 
 				  widthList[0], heightList[0], depthList[0],
